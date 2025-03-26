1181ad9a0e54b092cf690d5c06ba7d0a37e7bff5
Wathma Herath <wathmaherath437@gmail.com>
2025-03-26 21:41:57 +0530

    Add files via upload

---
+++ Bday_cake.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+def birthdayCakeCandles(candles):
+    x = candles.count(max(candles))
+    print (x)
+    
+if __name__ == '__main__':
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
