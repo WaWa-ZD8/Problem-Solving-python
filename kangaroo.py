f455d0a33bc5dcb32dc56b7ea531bb9a69a3d660
Wathma Herath <wathmaherath437@gmail.com>
2025-03-31 20:06:58 +0530

    Add files via upload

---
+++ kangaroo.py
@@ -0,0 +1,20 @@
+def kangaroo(x1, v1, x2, v2):
+    if v1 > v2 and (x2 - x1) % (v1 - v2) == 0:
+        print("YES")
+    else:
+        print("NO")
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    kangaroo(x1, v1, x2, v2)
+
+
