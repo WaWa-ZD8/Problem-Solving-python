09eef71920c2f839582f8c3ebebb732d22fb1e1d
Wathma Herath <wathmaherath437@gmail.com>
2025-04-10 18:00:10 +0530

    Add files via upload

---
+++ DivisibleSum.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    new_arr =list(permutations(ar, 2))
+    for array in new_arr:
+        if sum(array) % k == 0:
+            count += 1
+    print(count // 2)
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
